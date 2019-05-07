0fb76cd38d0172625143c509fa93e3cdfbd72a74
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2019-05-07 21:31:48 +0530

    black

--- black.py
+++ black.py
@@ -8,22 +8,22 @@ import numpy as np
 clf = joblib.load("digits_cls.pkl")
 
 # Read the input image 
-im = cv2.imread("black.jpg")
-im = cv2.bitwise_not(im)
-# cv2.imshow("aa",im)
-# cv2.waitKey()
-# im = im[0:120,50:150]
-# cv2.imshow("aa",im)
-# cv2.waitKey()
+im = cv2.imread("frame0.jpg")
+im = im[0:120,50:150]
+cv2.imshow("aa",im)
+cv2.waitKey()
 # Convert to grayscale and apply Gaussian filtering
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 # im_gray = cv2.GaussianBlur(im_gray, (5, 5), 1)
+im_gray = cv2.bitwise_not(im_gray)
+# cv2.imshow("aa",im_gray)
+# cv2.waitKey()
 # Threshold the image
 ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
 
 # Find contours in the image
 ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+# cv2.imshow("aa",im_gray)
 # Get rectangles contains each contour
 rects = [cv2.boundingRect(ctr) for ctr in ctrs]
 
@@ -33,22 +33,14 @@ for rect in rects:
     # Draw the rectangles
     cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
     # Make the rectangular region around the digit
-    leng = int(rect[3] * 40 )
+    leng = int(rect[3] * 40)
     pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
     pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
     roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
-    cv2.imshow("aa",im_th)
+    cv2.imshow("aa",roi)
+    cv2.imwrite("black.jpg",roi)
     cv2.waitKey()
-    # Resize the image
-    # print (roi.shape)
-    # print(roi)
-    # roi = cv2.resize(roi,(28,28),  interpolation=cv2.INTER_AREA)
-    roi = cv2.dilate(im_th, (3, 3))
-    # Calculate the HOG features
-    roi_hog_fd = hog(im_th, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualize=False)
-    nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
-    cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+    break
 
 cv2.imshow("Resulting Image with Rectangular ROIs", im)
 cv2.waitKey()
-cv2.destroyallwindows()
